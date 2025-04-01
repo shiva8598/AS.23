a607336c71f3a57381e4daacc1e6bf62b874c11f
shiva8598 <shivagautam8598@gmail.com>
2025-04-01 10:28:03 +0530

    day1

---
+++ day1/add.js
@@ -0,0 +1,13 @@
+add.js file 
+const add = (a,b)=>{
+    return a+b; //not joisted
+}
+const sub = (a,b)=>{
+    return a-b;
+}    
+module.exports = {add,sub};
+//module.exports = sub;
+//console.log(addWithArrow(6,7));
+//function add(a,b){
+// return a+b; //hoisted
+//}}
---
+++ day1/hhhh.js
@@ -0,0 +1,5 @@
+indexjs file 
+//const calc = require('./add.js');
+import {calc} from './Education/pdhai.html/add.js';
+console.log(calc.add(3,7));
+console.log(calc.sub(6,7));
